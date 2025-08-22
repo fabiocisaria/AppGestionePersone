e80f618f2f8506b18cfa20ac01c4d0fc34947735
Fabio Cisaria <fabio.cisaria@gmail.com>
2025-08-22 11:59:34 +0200

    Creata Form visita uro - ginecologica

--- AppTestGestionePersone/FormVisitaUroGineco.Designer.vb
+++ AppTestGestionePersone/FormVisitaUroGineco.Designer.vb
@@ -22,9 +22,16 @@ Partial Class FormVisitaUroGineco
     'Non modificarla mediante l'editor del codice.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'FormVisitaUroGineco
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "FormVisitaUroGineco"
+        Me.Name = "FormVisitaUroGineco"
+        Me.Text = "Visita uro - ginecologica"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
